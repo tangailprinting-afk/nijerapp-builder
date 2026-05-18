a9d7a517dea8fa94d754afef45f79ba680269b6e
tangailprinting-afk <tangailprinting@gmail.com>
2026-05-18 14:13:51 +0600

    updated package

--- android-template/WebAppEngine/app/build.gradle.kts
+++ android-template/WebAppEngine/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.webapp.engine"
+        applicationId = "nijerapp.amarapp.app"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
