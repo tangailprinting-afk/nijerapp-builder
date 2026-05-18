173239f09e559bfd45d62c0112b6553a4c9c3c49
tangailprinting-afk <tangailprinting@gmail.com>
2026-05-18 15:30:17 +0600

    updated package

--- android-template/WebAppEngine/app/build.gradle.kts
+++ android-template/WebAppEngine/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.sdfsdfsdf.app"
+        applicationId = "com.onesignaltest.app"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
