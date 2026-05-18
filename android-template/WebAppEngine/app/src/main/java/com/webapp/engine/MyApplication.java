8d3d0a56de0edfbb1d1c9ec48b40b1c137f69807
tangailprinting-afk <tangailprinting@gmail.com>
2026-05-18 15:53:27 +0600

    fixed onesignal initialize

--- android-template/WebAppEngine/app/src/main/java/com/webapp/engine/MyApplication.java
+++ android-template/WebAppEngine/app/src/main/java/com/webapp/engine/MyApplication.java
@@ -12,11 +12,16 @@ extends Application {
 
         super.onCreate();
 
-        OneSignal.initWithContext(this);
+        OneSignal.initialize(
 
-        OneSignal.setAppId(
+            this,
+
 "cf9a26bb-42ee-439b-a8d1-bb3ca6ca6d06"
+
         );
+
+        OneSignal.getNotifications()
+            .requestPermission(false);
 
     }
 
