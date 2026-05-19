e982a29617d16ee1ffd431a272ec4a9ca291925b
tangailprinting-afk <tangailprinting@gmail.com>
2026-05-19 15:15:04 +0600

    updated package

--- android-template/WebAppEngine/app/build.gradle.kts
+++ android-template/WebAppEngine/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.bbbb.app"
+        applicationId = "com.vercel.app"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
