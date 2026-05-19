a70ccfad4c5dfa11cf82a30e690ed0654de6613b
tangailprinting-afk <tangailprinting@gmail.com>
2026-05-19 13:35:23 +0600

    updated package

--- android-template/WebAppEngine/app/build.gradle.kts
+++ android-template/WebAppEngine/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.mimmiah.app"
+        applicationId = "com.ssssssssss.app"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
