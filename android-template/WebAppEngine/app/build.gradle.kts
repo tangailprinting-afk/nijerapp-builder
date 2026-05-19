da28da9081227c118b76a4024ef9ba16be21f25c
tangailprinting-afk <tangailprinting@gmail.com>
2026-05-19 17:51:49 +0600

    updated package

--- android-template/WebAppEngine/app/build.gradle.kts
+++ android-template/WebAppEngine/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "calculatorfinal.app.app"
+        applicationId = "last.app.com"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
