8675a48ab78cf94a0a7e785b3f8a02543dd3416b
tangailprinting-afk <tangailprinting@gmail.com>
2026-05-19 16:52:32 +0600

    updated package

--- android-template/WebAppEngine/app/build.gradle.kts
+++ android-template/WebAppEngine/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "test.test.test"
+        applicationId = "com.calculatoramar.app"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
