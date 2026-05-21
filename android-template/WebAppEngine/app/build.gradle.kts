998a3bde017aaecf120c93afcc16a182addd1e80
tangailprinting-afk <tangailprinting@gmail.com>
2026-05-21 16:22:23 +0600

    updated package

--- android-template/WebAppEngine/app/build.gradle.kts
+++ android-template/WebAppEngine/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.calculator.app"
+        applicationId = "com.gonitai.app"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
