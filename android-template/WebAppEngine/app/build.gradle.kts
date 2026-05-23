514b286668d75776c9630ff6853cddd4e40521e6
tangailprinting-afk <tangailprinting@gmail.com>
2026-05-23 19:23:45 +0600

    updated package

--- android-template/WebAppEngine/app/build.gradle.kts
+++ android-template/WebAppEngine/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.motingsir.app"
+        applicationId = "sir.motinsir.app"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
