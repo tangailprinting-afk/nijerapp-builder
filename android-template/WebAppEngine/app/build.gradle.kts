0ccc56092b032f070fba0662ffef310ca1a734ca
tangailprinting-afk <tangailprinting@gmail.com>
2026-06-21 20:33:01 +0600

    updated package

--- android-template/WebAppEngine/app/build.gradle.kts
+++ android-template/WebAppEngine/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "com.sultalsing.app"
+        applicationId = "digital.sultan.app"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
