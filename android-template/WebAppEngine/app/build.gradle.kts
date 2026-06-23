ad00fb28aaee5f39aa4785dbf925ddc80c4036e0
tangailprinting-afk <tangailprinting@gmail.com>
2026-06-23 12:10:59 +0600

    updated package

--- android-template/WebAppEngine/app/build.gradle.kts
+++ android-template/WebAppEngine/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "digital.sultan.app"
+        applicationId = "rizq.restaurent.app"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
