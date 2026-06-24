b53a21c5c19aefdd12cd56bf61d5a808a8e3fab6
tangailprinting-afk <tangailprinting@gmail.com>
2026-06-24 20:40:16 +0600

    updated package

--- android-template/WebAppEngine/app/build.gradle.kts
+++ android-template/WebAppEngine/app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     }
 
     defaultConfig {
-        applicationId = "app.rizeeq.app"
+        applicationId = "sdfsdf.sdfsdfsdf.app"
         minSdk = 24
         targetSdk = 36
         versionCode = 1
